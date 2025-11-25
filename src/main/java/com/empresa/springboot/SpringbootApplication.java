4487f82010dcba9d6b094ccf0bd4f79049eb2f97
Joaquin Bizcocho Cano <jbizcan399@g.educaand.es>
2025-11-25 10:32:43 +0100

    proyecto creado

---
+++ src/main/java/com/empresa/springboot/SpringbootApplication.java
@@ -0,0 +1,15 @@
+
+package com.empresa.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplication.class, args);
+		System.out.println("Hola Spring Boot");
+	}
+
+}
