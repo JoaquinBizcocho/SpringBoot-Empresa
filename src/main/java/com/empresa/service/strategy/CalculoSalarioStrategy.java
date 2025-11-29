aaeb90b6ef7536524d1682dae6e4fcf3072e2c08
Joaquin Bizcocho <jbizcan399@g.educaand.es>
2025-11-29 19:16:11 +0100

    Delete src directory

--- src/main/java/com/empresa/service/strategy/CalculoSalarioStrategy.java
+++
@@ -1,8 +0,0 @@
-package com.empresa.service.strategy;
-
-import com.empresa.model.Empleado;
-
-// Interfaz que define la estrategia de cálculo del salario
-public interface CalculoSalarioStrategy {
-    double calcularSalario(Empleado empleado);
-}
